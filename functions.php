f9aba61eebe4a9444f1eaabc8adbf8f2aa8d21c4
Iason Myttas <iasonasm@gmail.com>
2017-02-24 10:34:30 +0000

    Printed cards, calculated scores and declared winner. Also made a small change to arrays.

--- functions.php
+++ functions.php
@@ -1,3 +1,48 @@
 <?php
-    echo "This is Jason's part"
+    
+    include 'silverjack.php';
+    
+    // echo "This is Jason's part";
+    
+
+    function drawcards($cardArray, &$playerScoreArray)
+    {
+        $scores = array('');
+        foreach ($playerScoreArray as $name => $score) //go through all players
+        {
+            while($score < 42) //check if the current player's score is not already above 42
+            {
+                $key = array_rand($cardArray); //pick a random card
+                if($score + $cardArray[$key] < 42) //check if this random card's score would make the player's score higher than 42
+                { //if no then continue
+                    echo "<img src= \"" . $key . "\" /> "; //print card
+                    $score = $score +  $cardArray[$key]; //update score
+                    unset($cardArray[$key]); //remove card from the array
+                }
+                else //if yes, end current player
+                    break;
+            }
+            echo $score; //current player's total score
+            array_push($scores, $score); //array of scores
+            echo "</br>";
+        }
+        
+        displaywinner($scores); //call second function
+
+    }
+
+    
+    function displaywinner($playerScoreArray)
+    {
+        //find max in array of scores
+        echo "And the winner is......";
+        $max = 0;
+        foreach ($playerScoreArray as $name => $score) //go through all players
+           if($score > $max)
+                $max = $score; //update max
+                
+        echo $max;
+    }
+    drawcards($cardArray, $playerScoreArray); //call first function
+    
 ?>
